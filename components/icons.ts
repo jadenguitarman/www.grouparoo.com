db3ed208759c35f92883385dae451c2e5d5e06e4
Teal Larson <teal.larson@grouparoo.com>
2021-05-10 15:47:16 -0700

    added categories

--- components/icons.ts
+++ components/icons.ts
@@ -9,12 +9,15 @@ import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
 import { faBalanceScaleLeft } from "@fortawesome/free-solid-svg-icons";
 import { faBatteryFull } from "@fortawesome/free-solid-svg-icons";
 import { faWrench } from "@fortawesome/free-solid-svg-icons";
+import { faBuilding } from "@fortawesome/free-solid-svg-icons";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import { faChartLine } from "@fortawesome/free-solid-svg-icons";
 import { faVial } from "@fortawesome/free-solid-svg-icons";
 import { faTasks } from "@fortawesome/free-solid-svg-icons";
 import { faLayerGroup } from "@fortawesome/free-solid-svg-icons";
 import { faShoePrints } from "@fortawesome/free-solid-svg-icons";
 import { faPuzzlePiece } from "@fortawesome/free-solid-svg-icons";
+import { faUserTie } from "@fortawesome/free-solid-svg-icons";
 
 library.add(
   faLink,
@@ -25,10 +28,16 @@ library.add(
   faBalanceScaleLeft,
   faBatteryFull,
   faWrench,
+  faBuilding,
+  faCheck,
   faChartLine,
   faVial,
   faTasks,
   faLayerGroup,
   faShoePrints,
-  faPuzzlePiece
+  faPuzzlePiece,
+  faTasks,
+  faUserTie,
+  faVial,
+  faWrench
 );
